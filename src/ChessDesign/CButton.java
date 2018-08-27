2a890cfab8ec053db1812d17ff2e5e0480bc3e35
dbrill <dabr9981@colorado.edu>
2018-08-27 15:56:17 -0600

    explicit gitignore and extended CButton class

--- src/ChessDesign/CButton.java
+++ src/ChessDesign/CButton.java
@@ -1,4 +1,15 @@
 package ChessDesign;
+import Pieces.Piece;
+import javax.swing.*;
 
-public class CButton {
+public class CButton extends JButton  {
+    Piece piece;
+
+    public CButton(Piece piece){
+        super(piece.displayText);
+        this.piece = piece;
+    }
+    public CButton(){
+        super("");
+    }
 }
