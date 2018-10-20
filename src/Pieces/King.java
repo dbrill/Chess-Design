b4bc86052ff6fed3e8b6a1d1a92e5a1e08854e7a
dbrill <dabr9981@colorado.edu>
2018-10-19 18:13:42 -0600

    Implement King

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -1,6 +1,8 @@
 package Pieces;
 
 import ChessDesign.Player;
+import ChessDesign.Board;
+
 
 /**
  * @author Abhijit on 3/17/2018
@@ -15,15 +17,23 @@ public class King extends Piece{
 	 * @param y
 	 */
 	public King(Player player, int x, int y) {
-		super(Type.QUEEN, player, x, y);
+		super(Type.KING, player, x, y);
 		
 		this.displayText += "K";
 	}
 
 	@Override
 	public boolean movement_type(int fx, int fy) {
-		// TODO Auto-generated method stub
-		
+		if(Math.abs(this.x - fx) > 1 || Math.abs(this.y - fy) > 1){
+			System.out.println("Kings can only move one space at a time!");
+			return false;
+		}
+
+		//TODO check to make sure king isn't moving into check(mate)
+		if(!Board.spaceIsSafe(fx, fy, this.player)){
+			System.out.println("You can't move your king into jeopardy!");
+			return false;
+		}
 		return true;
 	}
 
